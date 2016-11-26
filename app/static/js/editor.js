9e361fddf0992a76a55fbe43c59061f226605c0f
seagullbird <seagullbird823@gmail.com>
2016-11-26 20:32:00 +0800

    updated editor.js

--- app/static/js/editor.js
+++ app/static/js/editor.js
@@ -1,39 +1,3 @@
-	// title validation
-	function validate_title() {
-		// for testing
-		return true;
-		var 
-			re_title = /^[0-9a-zA-Z\_][0-9a-zA-Z\_\s]{0,49}$/,
-			title = $('#title');
-			if (re_title.test(title.val())) return true;
-			alert('Invalid title format.');
-			return false;
-	}
-
-	// Category validation
-	function validate_cat() {
-		// for testing
-		return true;
-		var
-			re_cat = /^[0-9a-zA-Z\.\_]{1,20}$/,
-			cat = $('#category');
-			if (re_cat.test(cat.val())) return true;
-			alert('Invalid category format.');
-			return false;
-	}
-
-	// tag validation
-	function valiadate_tag() {
-		// for testing
-		return true;
-		var
-			re_tag = /^[0-9a-zA-Z\_\,]{0,256}$/,
-			tags = $('#tags');
-			if (re_tag.test(tags.val())) return true;
-			alert('Invalid tags format.');
-			return false;
-	}
-
 	// text validation
 	function validate_text() {
 		// for testing
@@ -44,14 +8,14 @@
 	}
 
 	$('#publish').click(function() {
-		if (validate_title() && validate_cat() && valiadate_tag() && validate_text()) {
+		if (validate_text()) {
 			$('#draft').val(false);
 	    	$('#text_form').submit();
 		} 
 	});
 
 	$('#sava_draft').click(function() {
-		if (validate_title() && validate_cat() && valiadate_tag() && validate_text()) {
+		if (validate_text()) {
 			$('#draft').val(true);
 	    	$('#text_form').submit();
 		} 
