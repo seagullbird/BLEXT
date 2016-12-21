8f9987f2d0be15050aed40f1e95df4d2376a250a
seagullbird <seagullbird823@gmail.com>
2016-12-21 15:12:52 +0800

    fixed bug

--- app/auth/views.py
+++ app/auth/views.py
@@ -130,7 +130,7 @@ def password_reset_request():
         flash('An email with instructions to reset your password has been '
               'sent to you.')
         return redirect(url_for('auth.sign_in'))
-    return render_template('auth/reset_password.html', form=form)
+    return render_template('auth/reset_password.html', form=form, msg='Enter your email you registered when signed up, an confirmation email will be sent to you right away.')
 
 
 # 重设密码页面
@@ -148,4 +148,4 @@ def password_reset(token):
             return redirect(url_for('auth.sign_in'))
         else:
             return redirect(url_for('main.index'))
-    return render_template('auth/reset_password.html', form=form)
+    return render_template('auth/reset_password.html', form=form, msg='Please reset your password.')
